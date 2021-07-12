f8d1e27fbc192b79c2aced41d093b902cf350b1c
Aguinaldo Borges <74510307+AguinaldoBorges@users.noreply.github.com>
2021-07-12 12:30:18 -0300

    Adicionado database.js

---
+++ database.js
@@ -0,0 +1,69 @@
+const livros = [
+    {
+        id: 1,
+        nome: "Super memória para concursos",
+        autor: "Alberto Dell'Isola",
+        categoria: "Estudos",
+        páginas: 159,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 2,
+        nome: "Eu não Consigo Emagrecer",
+        autor: "Dr. Pierre Dukan",
+        categoria: "Saúde",
+        páginas: 282,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 3,
+        nome: "Quero Mudar",
+        autor: "Bernardo Stamateas",
+        categoria: "Auto-Ajuda",
+        páginas: 174,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 4,
+        nome: "Geração de Valor",
+        autor: "Flávio Augusto",
+        categoria: "Auto-Ajuda",
+        páginas: 207,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 5,
+        nome: "Pega a Visão",
+        autor: "Rick Chester",
+        categoria: "Auto-Ajuda",
+        páginas: 189,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 6,
+        nome: "Eu vou te ensinar a ser rico",
+        autor: "Ben Zruel",
+        categoria: "Auto-Ajuda",
+        páginas: 158,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 7,
+        nome: "Digital Minimalism",
+        autor: "Cal Newport",
+        categoria: "Produtividade",
+        páginas: 254,
+        recomenda: false,
+        leu: false
+    },
+
+];
+
+//Exportando os dados 
+module.exports = livros
